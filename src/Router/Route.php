d21f5203ea7a5b36d22050e8d2e91a7c72b69572
WolfGang <flamin2k8@gmail.com>
2019-03-11 23:46:23 +0000

    return array

--- src/Router/Route.php
+++ src/Router/Route.php
@@ -255,7 +255,7 @@ class Route
     /**
      * @return mixed
      */
-    public function getSdkProperties(): ?RouteSDKProperties
+    public function getSdkProperties(): ?array
     {
         return $this->sdkProperties;
     }
